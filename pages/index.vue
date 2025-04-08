4db3e663b4c9effb5bff80f81284b0c72cb21479
downtomarsguy <downtomarsguy@gmail.com>
2025-04-08 15:43:47 +0530

    On enter -> Verifier Added

--- pages/index.vue
+++ pages/index.vue
@@ -98,6 +98,87 @@ export default {
                 }
             } else if (key === "Backspace") {
                 this.pressedKey = this.pressedKey.slice(0, -1);
+            } else if (key === "Enter") {
+                this.checkAnswer();
+            }
+        },
+        checkAnswer() {
+            const russianHashMap = {
+                А: "A",
+                Б: "B",
+                В: "V",
+                Г: "G",
+                Д: "D",
+                Е: "E",
+                Ё: "Yo",
+                Ж: "Zh",
+                З: "Z",
+                И: "I",
+                Й: "Y",
+                К: "K",
+                Л: "L",
+                М: "M",
+                Н: "N",
+                О: "O",
+                П: "P",
+                Р: "R",
+                С: "S",
+                Т: "T",
+                У: "U",
+                Ф: "F",
+                Х: "Kh",
+                Ц: "Ts",
+                Ч: "Ch",
+                Ш: "Sh",
+                Щ: "Shch",
+                Ъ: "'",
+                Ы: "Y",
+                Ь: "'",
+                Э: "E",
+                Ю: "Yu",
+                Я: "Ya",
+                а: "a",
+                б: "b",
+                в: "v",
+                г: "g",
+                д: "d",
+                е: "e",
+                ё: "yo",
+                ж: "zh",
+                з: "z",
+                и: "i",
+                й: "y",
+                к: "k",
+                л: "l",
+                м: "m",
+                н: "n",
+                о: "o",
+                п: "p",
+                р: "r",
+                с: "s",
+                т: "t",
+                у: "u",
+                ф: "f",
+                х: "kh",
+                ц: "ts",
+                ч: "ch",
+                ш: "sh",
+                щ: "shch",
+                ъ: "'",
+                ы: "y",
+                ь: "'",
+                э: "e",
+                ю: "yu",
+                я: "ya",
+            };
+
+            const correctAnswer = russianHashMap[this.randomLetter];
+
+            if (this.pressedKey === correctAnswer) {
+                this.pressedKey = "";
+                this.randomLetter = this.randomizeLetter();
+            } else {
+                console.log("Nuh uh");
             }
         },
     },
